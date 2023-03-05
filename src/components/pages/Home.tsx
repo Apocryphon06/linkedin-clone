bf531f4e7d721d2bd338013ebf137f4662e90ed1
Apocryphon06 <hrithikchandrashekar@gmail.com>
2023-03-05 21:21:17 +0530

    homepage layout

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,8 +2,20 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
-      Home
+    <div className="grid grid-flow-col w-[1190px] m-auto pt-5">
+
+      <div className="w-[270px] bg-white rounded-lg h-[500px] shadow-md">
+        <p>Profile Card</p>
+      </div>
+
+      <div className="w-[570px] bg-white rounded-lg shadow-md h-[100vh]">
+        <p>Feeds</p>
+      </div>
+
+      <div className="w-[270px] bg-white rounded-lg h-[500px] shadow-md">
+        <p>Linkedin News</p>
+      </div>
+      
     </div>
   );
 }
