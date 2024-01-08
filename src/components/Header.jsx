f269d6eb8b85a663a3b544272020eb6413b8c8aa
Apocryphon06 <hrithikchandrashekar@gmail.com>
2024-01-08 14:37:18 +0530

    jobs & networks page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { Fragment } from 'react'
 
 const Header = () => {
-  return <div className="h-12 bg-[#808080] w-full flex items-center justify-start lg:px-40 md:px-6 px-4 fixed top-0 z-[98]">Header</div>
+  const Router = useRouter()
+
+  return (
+    <Fragment>
+      {Router.pathname !== '/' && (
+        <div className="h-12 bg-[#222222] text-white w-full flex items-center justify-start lg:px-40 md:px-6 px-4 fixed top-0 z-[98]">
+          Header
+        </div>
+      )}
+    </Fragment>
+  )
 }
 
 export default Header
--- src/components/Messaging.jsx
+++ src/components/Messaging.jsx
@@ -1,40 +1,47 @@
-import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import React, { Fragment, useState } from 'react'
 
 const Messaging = () => {
   const [menu, setMenu] = useState(false)
 
+  const Router = useRouter()
+
   return (
-    <div
-      className={`w-3/12 bg-[#808080] ${
-        !menu ? 'h-14' : 'h-[80vh]'
-      } fixed bottom-0 right-2 rounded-tl-lg transition-all ease-linear duration-100 rounded-tr-lg flex items-start justify-center p-4`}
-    >
-      <div className="flex items-center w-full justify-between">
-        <p className="text-base font-medium">Messagining</p>
+    <Fragment>
+      {Router.pathname !== '/' && (
+        <div
+          className={`w-3/12 bg-cyan-100 ${
+            !menu ? 'h-14' : 'h-[80vh]'
+          } fixed bottom-0 right-2 rounded-tl-lg transition-all ease-linear duration-100 rounded-tr-lg flex items-start justify-center p-4`}
+        >
+          <div className="flex items-center w-full justify-between">
+            <p className="text-base font-medium">Messagining</p>
 
-        <div className="flex items-center gap-6">
-          <button>
-            <img src="/icons/write.svg" alt="write" className="w-4 h-4" />
-          </button>
+            <div className="flex items-center gap-6">
+              <button>
+                <img src="/icons/write.svg" alt="write" className="w-4 h-4" />
+              </button>
 
-          <button>
-            <img
-              src="/icons/threeDots.svg"
-              alt="threeDots"
-              className="w-4 h-4"
-            />
-          </button>
+              <button>
+                <img
+                  src="/icons/threeDots.svg"
+                  alt="threeDots"
+                  className="w-4 h-4"
+                />
+              </button>
 
-          <button onClick={() => setMenu(!menu)} className="">
-            <img
-              src="/icons/upArrow.svg"
-              alt="upArrow"
-              className={`w-4 h-4 ${!menu ? '' : '-rotate-180'}`}
-            />
-          </button>
+              <button onClick={() => setMenu(!menu)} className="">
+                <img
+                  src="/icons/upArrow.svg"
+                  alt="upArrow"
+                  className={`w-4 h-4 ${!menu ? '' : '-rotate-180'}`}
+                />
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </Fragment>
   )
 }
 
