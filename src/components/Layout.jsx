3880d8771f33d4c1d4769bb93c4813e1af973435
Apocryphon06 <hrithikchandrashekar@gmail.com>
2024-04-18 15:50:20 +0530

    clean up

--- src/components/Layout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Layout = ({ children }) => {
-  return (
-    <div className="lg:px-40 md:px-6 px-3 w-full min-h-screen">{children}</div>
-  )
-}
-
-export default Layout
